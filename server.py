17d31657ea3083e09cfdb0eb0c54d2055b06a6e4
[redacted] <r3tr0_02@protonmail.com>
2024-03-20 00:10:59 +0800

    Test v1.1
    - Add GUI for login or register page
    - Add GUI for each login and register page
    - Code formatting

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+'''
 import socket
 import threading
 
@@ -50,6 +51,144 @@ def receive():
 
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
+
+print("Server is running!")
+receive()
+'''
+
+# `TODO : switch hashlib to argon2 properly
+# `TODO : start initial encryption between server-client in handling login/register inputs
+
+import socket
+import threading
+import sqlite3
+import hashlib
+import secrets
+
+HOST = '127.0.0.1'
+PORT = 9090
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+
+server.listen()
+
+clients = []
+nicknames = []
+
+# Create SQLite database and connect
+conn = sqlite3.connect('chat_users.db')
+cursor = conn.cursor()
+
+# Create table to store usernames, hashed passwords, and salts
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT,
+        password TEXT,
+        salt TEXT
+    )
+''')
+conn.commit()
+
+def generate_salt():
+    return secrets.token_hex(16)
+
+def hash_password(password, salt):
+    return hashlib.sha256((password + salt).encode('utf-8')).hexdigest()
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            
+            # ! remove logging in server - based
+            #print(f"{nicknames[clients.index(client)]} says {message}")
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+
+            nickname = nicknames[index]
+            nicknames.remove(nickname)
+
+            break
+
+def receive():
+    while True:
+        client, address = server.accept()
+        print(f"Connected with {str(address)}!")
+
+        # Ask for login or register
+        
+        # ! remove send message to client
+        #client.send("LOGIN_OR_REGISTER".encode('utf-8'))
+        login_or_register = client.recv(1024).decode('utf-8')
+
+        if login_or_register.lower() == 'login':
+
+            # ! remove send message to client
+            #client.send("LOGIN".encode('utf-8'))
+            login_data = client.recv(1024).decode('utf-8').split()
+            username = login_data[0]
+            password = login_data[1]
+
+            # Retrieve salt for the user
+            cursor.execute('SELECT salt FROM users WHERE username = ?', (username,))
+            salt_data = cursor.fetchone()
+
+            if salt_data:
+                salt = salt_data[0]
+                hashed_password = hash_password(password, salt)
+
+                # Verify login credentials
+                cursor.execute('SELECT username, password FROM users WHERE username = ? AND password = ?', (username, hashed_password))
+                user_data = cursor.fetchone()
+
+                if user_data:
+                    client.send("LOGIN_SUCCESS".encode('utf-8'))
+
+                    # Set the username as the nickname
+                    nicknames.append(username)
+
+                    clients.append(client)
+
+                    print(f"Nickname of the client is {username}")
+                    broadcast(f"{username} connected to the server!\n".encode('utf-8'))
+                    client.send("Connected to the server".encode('utf-8'))
+
+                    thread = threading.Thread(target=handle, args=(client,))
+                    thread.start()
+                else:
+                    client.send("LOGIN_FAILED".encode('utf-8'))
+            else:
+                client.send("LOGIN_FAILED".encode('utf-8'))
+        elif login_or_register.lower() == 'register':
+
+            # ! remove send message to client
+            #client.send("REGISTER".encode('utf-8'))
+            register_data = client.recv(1024).decode('utf-8').split()
+            username = register_data[0]
+            password = register_data[1]
+
+            # Generate salt
+            salt = generate_salt()
+
+            # Hash password
+            hashed_password = hash_password(password, salt)
+
+            # Insert new user into the database
+            cursor.execute('INSERT INTO users (username, password, salt) VALUES (?, ?, ?)', (username, hashed_password, salt))
+            conn.commit()
+
+            client.send("REGISTER_SUCCESS".encode('utf-8'))
+        else:
+            client.send("INVALID_OPTION".encode('utf-8'))
 
 print("Server is running!")
 receive()
--- testGui.py
+++ testGui.py
@@ -1,3 +1,4 @@
+'''
 import socket
 import threading
 import tkinter as tk
@@ -67,6 +68,298 @@ class Client:
         self.sock.close()
         exit(0)
 
+    def receive(self):
+        while self.running:
+            try:
+                message = self.sock.recv(1024).decode('utf-8')
+                if message == 'NICK':
+                    self.sock.send(self.nickname.encode('utf-8'))
+                else:
+                    if self.gui_done:
+                        self.text_area.config(state='normal')
+                        self.text_area.insert('end', message)
+                        self.text_area.yview('end')
+                        self.text_area.config(state='disabled')
+            except ConnectionAbortedError:
+                break
+            except:
+                print("Error")
+                self.sock.close()
+                break
+
+client = Client(HOST, PORT)
+'''
+
+# `TODO : add GUI for login or register
+# `TODO : add GUI for username & password 
+
+import socket
+import threading
+import tkinter as tk
+import tkinter.scrolledtext
+from tkinter import simpledialog
+from tkinter import messagebox
+
+HOST = '127.0.0.1'
+PORT = 9090
+
+class Client:
+
+    def __init__(self, host, port):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((host, port))
+
+        self.login_or_register_gui()
+
+    # * This function is to ask users whether to login or register
+    def login_or_register_gui(self):
+        self.login_or_register_win = tk.Tk()
+        self.login_or_register_win.title("Login/Register")
+        self.login_or_register_win.configure(bg="lightgreen")
+
+        width = 600
+        height = 400
+        screenwidth = self.login_or_register_win.winfo_screenwidth()
+        screenheight = self.login_or_register_win.winfo_screenheight()
+        alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
+        self.login_or_register_win.geometry(alignstr)
+
+
+        self.login_button = tk.Button(self.login_or_register_win, text="Login", command=self.login_gui)
+        self.login_button.config(font=("Arial", 12))
+        self.login_button.pack(padx=20, pady=10)
+
+        self.register_button = tk.Button(self.login_or_register_win, text="Register", command=self.register_gui)
+        self.register_button.config(font=("Arial", 12))
+        self.register_button.pack(padx=20, pady=10)
+
+        self.login_or_register_win.protocol("WM_DELETE_WINDOW", self.stop_login_register)
+
+        self.login_or_register_win.mainloop()
+
+    # * This function is to ask users for username and password for login
+    def login_gui(self):
+        self.login_or_register_win.destroy()
+        self.sock.send("login".encode('utf-8'))
+
+        self.login_win = tk.Tk()
+        self.login_win.title("Login")
+        self.login_win.configure(bg="lightgreen")
+
+        width = 600
+        height = 400
+        screenwidth = self.login_win.winfo_screenwidth()
+        screenheight = self.login_win.winfo_screenheight()
+        alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
+        self.login_win.geometry(alignstr)
+
+        self.username_label = tk.Label(self.login_win, text="Username : ", bg="lightgreen")
+        self.username_label.config(font=("Arial", 12))
+        self.username_label.pack(padx=20, pady=5)
+
+        self.username_input = tk.Text(self.login_win, height=3)
+        self.username_input.pack(padx=20, pady=5)
+
+        self.password_label = tk.Label(self.login_win, text="Password :", bg="lightgreen")
+        self.password_label.config(font=("Arial", 12))
+        self.password_label.pack(padx=20, pady=5)
+
+        self.password_input = tk.Text(self.login_win, height=3)
+        self.password_input.pack(padx=20, pady=5)
+
+        self.send_button = tk.Button(self.login_win, text="Login", command=self.login)
+        self.send_button.config(font=("Arial", 12))
+        self.send_button.pack(padx=20, pady=5)
+
+        self.login_win.protocol("WM_DELETE_WINDOW", self.stop_login)
+
+        self.login_win.mainloop()
+
+    # * This function is to ask users for username, password and confirm
+    # * password for register
+    def register_gui(self):
+        self.login_or_register_win.destroy()
+        self.sock.send("register".encode('utf-8'))
+
+        self.register_win = tk.Tk()
+        self.register_win.title("Login")
+        self.register_win.configure(bg="lightgreen")
+
+        width = 600
+        height = 400
+        screenwidth = self.register_win.winfo_screenwidth()
+        screenheight = self.register_win.winfo_screenheight()
+        alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
+        self.register_win.geometry(alignstr)
+
+        self.username_label = tk.Label(self.register_win, text="Username : ", bg="lightgreen")
+        self.username_label.config(font=("Arial", 12))
+        self.username_label.pack(padx=20, pady=5)
+
+        self.username_input = tk.Text(self.register_win, height=3)
+        self.username_input.pack(padx=20, pady=5)
+
+        self.password_label = tk.Label(self.register_win, text="Password :", bg="lightgreen")
+        self.password_label.config(font=("Arial", 12))
+        self.password_label.pack(padx=20, pady=5)
+
+        self.password_input = tk.Text(self.register_win, height=3)
+        self.password_input.pack(padx=20, pady=5)
+
+        self.confirm_password_label = tk.Label(self.register_win, text="Password :", bg="lightgreen")
+        self.confirm_password_label.config(font=("Arial", 12))
+        self.confirm_password_label.pack(padx=20, pady=5)
+
+        self.confirm_password_input = tk.Text(self.register_win, height=3)
+        self.confirm_password_input.pack(padx=20, pady=5)
+
+        self.send_button = tk.Button(self.register_win, text="Login", command=self.register)
+        self.send_button.config(font=("Arial", 12))
+        self.send_button.pack(padx=20, pady=5)
+
+        self.register_win.protocol("WM_DELETE_WINDOW", self.stop_register)
+
+        self.register_win.mainloop()
+
+    # * This function is to get input from login_gui funct and send to
+    # * server for verification
+    # ? After login, this function will goto message_gui
+    # ! If login fail, this function will goto login_or_register_gui
+    def login(self):
+        self.username = self.username_input.get('1.0', 'end-1c')
+        self.password = self.password_input.get('1.0', 'end-1c')
+
+        # ! input might get sanitized here
+
+        login_data = f"{self.username} {self.password}"
+        self.sock.send(login_data.encode('utf-8'))
+
+        # Wait for server response
+        login_response = self.sock.recv(1024).decode('utf-8')
+
+        if login_response == "LOGIN_SUCCESS":
+            self.login_win.destroy()
+
+            self.nickname = self.username
+            self.gui_done = False
+            self.running = True
+
+            gui_thread = threading.Thread(target=self.message_gui)
+            receive_thread = threading.Thread(target=self.receive)
+
+            gui_thread.start()
+            receive_thread.start()
+        else:
+            messagebox.showerror(title="Error", message="Your username and/or password is incorrect!")
+            self.login_win.destroy()
+
+            self.login_or_register_gui()
+
+    # * This function is to get input from register_gui funct and send to
+    # * server for verification
+    # ? After register, this function will goto login_or_register_gui
+    # ! If login fail, this function will goto login_or_register_gui
+    def register(self):
+        self.username = self.username_input.get('1.0', 'end-1c')
+        self.password = self.password_input.get('1.0', 'end-1c')
+        self.confirm_password = self.confirm_password_input.get('1.0', 'end-1c')
+
+        if self.password == self.confirm_password:
+            register_data = f"{self.username} {self.password}"
+            self.sock.send(register_data.encode('utf-8'))
+
+            # Wait for server response
+            register_response = self.sock.recv(1024).decode('utf-8')
+            if register_response == "REGISTER_SUCCESS":
+                messagebox.showinfo(title="Info", message="Registration complete. Please log in using your username and password.")
+                self.register_win.destroy()
+            else:
+                tk.messagebox.showerror(title="Error", message="Registration Failed!")
+                self.register_win.destroy()
+
+                self.login_or_register_gui()
+        else:
+            tk.messagebox.showerror(title="Error", message="Please double confirm your password!")
+            self.register_win.destroy()
+
+            self.login_or_register_gui()
+
+    # * This function is to get input from user message and display message
+    # * from other users
+    def message_gui(self):
+        self.win = tk.Tk()
+        self.login_win.title("Message")
+        self.win.configure(bg="lightgreen")
+
+        width = 600
+        height = 575
+        screenwidth = self.win.winfo_screenwidth()
+        screenheight = self.win.winfo_screenheight()
+        alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
+        self.win.geometry(alignstr)
+
+        self.chat_label = tk.Label(self.win, text="Chat:", bg="lightgreen")
+        self.chat_label.config(font=("Arial", 12))
+        self.chat_label.pack(padx=20, pady=5)
+
+        self.text_area = tk.scrolledtext.ScrolledText(self.win)
+        self.text_area.pack(padx=20, pady=5)
+        self.text_area.config(state='disabled')
+        
+        self.msg_label = tk.Label(self.win, text="Message:", bg="lightgreen")
+        self.msg_label.config(font=("Arial", 12))
+        self.msg_label.pack(padx=20, pady=5)
+
+        self.input_area = tk.Text(self.win, height=3)
+        self.input_area.pack(padx=20, pady=5)
+
+        self.send_button = tk.Button(self.win, text="Send", command=self.write)
+        self.send_button.config(font=("Arial", 12))
+        self.send_button.pack(padx=20, pady=5)
+
+        self.gui_done = True
+
+        self.win.protocol("WM_DELETE_WINDOW", self.stop)
+
+        self.win.mainloop()
+
+    # * This function is to send message from user to server for it to be 
+    # * broadcasted to other users
+    def write(self):
+        message = f"{self.nickname}: {self.input_area.get('1.0', 'end')}"
+        self.sock.send(message.encode('utf-8'))
+        self.input_area.delete('1.0', 'end')
+
+    # `* All stop funct below is to handle event where a window is closed
+    # ? might be a better way to do this...
+    def stop_login_register(self):
+        self.running = False
+        self.login_or_register_win.destroy()
+        self.sock.close()
+        exit(0)
+
+    def stop_login(self):
+        self.running = False
+        self.login_win.destroy()
+        self.sock.close()
+        exit(0)
+
+    def stop_register(self):
+        self.running = False
+        self.register_win.destroy()
+        self.sock.close()
+        exit(0)
+
+    def stop(self):
+        self.running = False
+        self.win.destroy()
+        self.sock.close()
+        exit(0)
+
+    # * This function is to receive message from server and display it in 
+    # * message_gui
+    # ! If conn is aborted OR error in message handling, the program will
+    # ! terminate conn and exit
     def receive(self):
         while self.running:
             try:
